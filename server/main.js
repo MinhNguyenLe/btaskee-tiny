dbae49bf5ca579f01f25a44a5a012ab01ea4c6a5
MinhLee2k <minh.nguyenle1809@gmail.com>
2022-12-22 14:51:09 +0700

    feat: Connect mongod

--- server/main.js
+++ server/main.js
@@ -5,27 +5,46 @@ async function insertLink({ title, url }) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
 }
 
+Meteor.publish("allLinks", () => {
+  return LinksCollection.find();
+})
+
 Meteor.startup(async () => {
-  // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
-    await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app',
-    });
+  // If the Links collection is empty, add some data
+  // if (await LinksCollection.find().countAsync() === 0) {
+  await insertLink({
+    title: 'Do the Tutorial',
+    url: 'https://www.meteor.com/tutorials/react/creating-an-app',
+  });
 
-    await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    });
+  await insertLink({
+    title: 'Follow the Guide',
+    url: 'https://guide.meteor.com',
+  });
 
-    await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    });
+  await insertLink({
+    title: 'Read the Docs',
+    url: 'https://docs.meteor.com',
+  });
 
-    await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
-    });
-  }
+  await insertLink({
+    title: 'Discussions',
+    url: 'https://forums.meteor.com',
+  });
+
+  await insertLink({
+    title: 'test 1',
+    url: 'https://forums.meteor.com',
+  });
+
+  await insertLink({
+    title: 'test 2',
+    url: 'https://forums.meteor.com',
+  });
+
+  await insertLink({
+    title: 'test 3',
+    url: 'https://forums.meteor.com',
+  });
+  // }
 });
